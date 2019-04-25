9c11f42142c80b19441d3b093c249f0b838799a3
Camilo Andres Concha Agudelo <cconcha@belatrix.com>
2019-04-25 10:41:42 -0500

    Added the prettier conf

--- 02-index.js
+++ 02-index.js
@@ -1,4 +1,4 @@
-const { graphql, buildSchema } = require("graphql");
+const { graphql, buildSchema } = require('graphql')
 const schema = buildSchema(`
 type Schema { query: Query }
 type Query { 
@@ -6,18 +6,18 @@ type Query {
   title: String,
   duration: Int, 
   released:Boolean }
-`);
+`)
 const query = `query myFirstQuery{
   id,title,duration,released 
-}`;
+}`
 
 const resolvers = {
-  id: () => "1",
-  title: () => "bar",
-  duration: () => 180,
-  released: () => true
-};
+    id: () => '1',
+    title: () => 'bar',
+    duration: () => 180,
+    released: () => true,
+}
 
 graphql(schema, query, resolvers)
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
